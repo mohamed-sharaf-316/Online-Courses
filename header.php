56be6a099b75033fe7a580a9c2b081d3578406d9
Mo'az Ismail <moazesmail882@gmail.com>
2020-06-09 20:00:34 +0200

    Student Web Design

--- header.php
+++ header.php
@@ -1,7 +1,28 @@
 <?php
+include("includes/config.php");
 error_reporting(0);
 ?>
+<?php if($_SESSION['login']!="")
+{?>
+<header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <strong>Welcome: </strong><?php echo htmlentities($_SESSION['sname']);?>
+                    &nbsp;&nbsp;
+                    
 
+
+                    <strong>Last Login:<?php 
+        $ret=mysqli_query($con,"SELECT  * from userlog where studentRegno='".$_SESSION['login']."' order by id desc limit 1,1");
+                    $row=mysqli_fetch_array($ret);
+                    echo $row['userip']; ?> at <?php echo $row['loginTime'];?></strong>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <?php } ?>
     <!-- HEADER END-->
     <div class="navbar navbar-inverse set-radius-zero">
         <div class="container">
@@ -11,7 +32,7 @@ error_reporting(0);
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                 <a class="navbar-brand" href="#" style="color:#fff; font-size:24px;4px; line-height:24px; ">
+                <a class="navbar-brand" href="#" style="color:#fff; font-size:24px;4px; line-height:24px; ">
 
                    Online Course Registration
                 </a>
--- menubar.php
+++ menubar.php
@@ -4,15 +4,10 @@
                 <div class="col-md-12">
                     <div class="navbar-collapse collapse ">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
-
-                            <li><a href="session.php">Session</a></li>
-                            <li><a href="semester.php">Semester </a></li>
-                            <li><a   href="department.php">Department</a></li>
-                             <li><a href="course.php">Course</a></li>
-                              <li><a href="student-registration.php">Registration</a></li>
-                               <li><a href="manage-students.php">Manage Students</a></li>
-                               <li><a href="enroll-history.php">Enroll History</a></li>
-                               <li><a href="user-log.php">Student Logs </a></li>
+                             <li><a href="pincode-verification.php">Enroll for Course </a></li>
+                             <li><a href="enroll-history.php">Enroll History  </a></li>
+                              <li><a href="my-profile.php">My Profile</a></li>
+                               <li><a href="change-password.php">Change Password</a></li>
                             <li><a href="logout.php">Logout</a></li>
 
                         </ul>
